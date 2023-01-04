2586268d6e63dff189f2986e03096af88746aed5
Trevor Mathiak <tmathiak@gmail.com>
2023-01-03 22:09:07 -0500

    implemented logout function

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,19 @@ var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get("/", function (req, res) {
+router.get("/", (req, res) => {
   res.render("index", { title: "Undercover Chat", user: req.user });
+});
+
+// user logout
+router.get("/logout", (req, res, next) => {
+  req.logout((err) => {
+    if (err) {
+      return next(err);
+    } else {
+      res.redirect("/");
+    }
+  });
 });
 
 module.exports = router;
