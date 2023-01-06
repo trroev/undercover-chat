211990539667ebdccfa0633e1461d24249a50bb9
Trevor Mathiak <tmathiak@gmail.com>
2023-01-06 13:22:01 -0500

    added compression and helmet dependencies

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const flash = require("connect-flash");
+const compression = require("compression");
+const helmet = require("helmet");
 
 // load environment variables from .env file
 require("dotenv").config();
@@ -67,6 +69,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(flash());
+app.use(compression());
+app.use(helmet());
 
 // set up routes
 app.use("/", indexRouter);
@@ -74,12 +78,12 @@ app.use("/users", usersRouter);
 app.use("/messages", messagesRouter);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
+app.use((req, res, next) => {
   next(createError(404));
 });
 
 // error handler
-app.use(function (err, req, res, next) {
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
